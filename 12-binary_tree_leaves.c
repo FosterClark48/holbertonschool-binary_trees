3791cb19981e782875e2fb8310c538f1c98630d2
Foster Clark <fosterclark48@yahoo.com>
2022-12-19 11:43:18 -0600

    Fixed space after return c:14

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return(1);
+		return (1);
 
 	l_leaf = binary_tree_leaves(tree->left);
 	r_leaf = binary_tree_leaves(tree->right);
