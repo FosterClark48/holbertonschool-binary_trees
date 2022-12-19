08eefbef7707eb3dfcea21897a325fec31bca720
Foster Clark <fosterclark48@yahoo.com>
2022-12-19 17:15:40 -0600

    updated helper function name

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,8 +15,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	if (binary_tree_height(tree->right) == binary_tree_height(tree->left))
 	{
-		right_height = binary_tree_height(tree->right);
-		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_is_perfect(tree->right);
+		left_height = binary_tree_is_perfect(tree->left);
 	}
 	else
 		return (0);
